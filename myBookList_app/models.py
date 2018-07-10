1cce5c1ff404216cb8bc886b2ab4443e468d2fa8
Mathéo Dumont <math.dumont.md@gmail.com>
2018-07-10 14:23:05 +0200

    Ajout dans models
    Les models Profil et userBook

--- myBookList_app/models.py
+++ myBookList_app/models.py
@@ -1,9 +1,12 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # à compléter
 choicesNationality = ("FR", "France"), ("US", "USA"), ("UK", "United Kingdom")
 
-choicesType = ("tri", "trilogie"), ("cyc", "cyle")
+choicesType = ("tri", "trilogie"), ("cyc", "cycle"), ("sin","single")
+
+stateUserBook = ("toRead", "à Lire"), ("abandoned", "abandonné"), ("pending", "en Attente"), ("onGoing", "en Cours")
 
 # ------------------------------  Models --------------------------------------
 
@@ -73,3 +76,35 @@ class SerieBook(models.Model):
 
     def __str__(self):
         return self.serie.name + " | tome " + str(self.num) + " : " + self.book.name
+
+# -----------------------------------------------------------------------------
+
+
+class Profil(models.Model):
+    user = models.OneToOneField(User, blank=True, null=True)
+    listGenres = list()
+
+    class Meta:
+        verbose_name = "Profil"
+        verbose_name_plural = "Profils"
+
+    def __str__(self):
+        return self.user.get_full_name()
+
+# -----------------------------------------------------------------------------
+
+
+class UserBook:
+    """
+    Le timestamp représente le timestamp de la dernière modification de 'state'
+    """
+    profil = models.ForeignKey(Profil, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.SET_NULL)
+    timestamp = models.DateField(auto_now=False, auto_now_add=True)
+    state = models.CharField(choices=stateUserBook, max_length=100)
+    note = models.IntegerField()
+    comment = models.CharField(max_length=1000)
+    readTimes = models.IntegerField()
+
+    def __str__(self):
+        return self.profil.__str__() + " " + self.book.__str__()
