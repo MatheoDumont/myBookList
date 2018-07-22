47ab3ca78e9651f77214da3cb676aa9e6305831d
Mathéo Dumont <math.dumont.md@gmail.com>
2018-07-22 17:11:50 +0200

    fix

---
+++ myBookList_app/views/IndexViews.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.contrib.auth.forms import AuthenticationForm
+from django.views import View
+
+from myBookList_app.forms.registrationForms import CustomUserCreationForm
+
+
+class Index(View):
+    template = 'index/index.html'
+
+    def get(self, request):
+        authform = AuthenticationForm()
+
+        return render(request, self.template, {
+            'authForm': authform,
+        })
